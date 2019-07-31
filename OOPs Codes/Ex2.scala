fc8b771c6846c8793d8a849cc76c0538dff61819
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-31 22:16:52 +0530

    Add files via upload

---
+++ OOPs Codes/Ex2.scala
@@ -0,0 +1,10 @@
+class Duck{
+var size = 1
+}
+
+object DuckDemo extends App {
+  var f = new Duck
+f.size = 10 // It calls f.size_=(10)
+//f.size_=(10) // It calls f.size_=(10)
+println(f.size) // It calls f.size()
+}
