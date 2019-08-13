8a4b064850ef1f2105bc1f024c7f2bf988d28f6c
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-08-13 23:16:55 +0530

    Add files via upload

---
+++ Traits Codes/Ex2.scala
@@ -0,0 +1,22 @@
+trait ConsoleLogger2 {
+  def log(msg: String) { println(msg) }
+}
+
+class Account2 {
+  protected var balance = 10.0
+}
+
+class SavingsAccount extends Account2 with ConsoleLogger2 {
+//class SavingsAccount extends ConsoleLogger2  with Account2{
+  def withdraw(amount: Double) {
+    if (amount > balance) log("Oops..! You don't have enough balance!!")
+    else balance -= amount
+  }
+
+  // More methods ...
+}
+
+object Main2 extends App {
+  val acct = new SavingsAccount
+  acct.withdraw(100)
+}
